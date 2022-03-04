7685c5a444f81db6a55941b3d4ae7d38acc4058b
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-04 14:38:43 +0200

    Last Added ve Footer componenti eklendi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './assets/style/style.css';
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import LastAdded from "./components/LastAdded/LastAdded";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <>
         <Navbar/>
         <Header/>
+        <LastAdded/>
+        <Footer/>
     </>
   );
 }
