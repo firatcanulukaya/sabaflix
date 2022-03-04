b92ea0a37d43e2242c235e7b7de8022941ff8f87
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-04 06:47:16 +0200

    Header ve Navbar eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './assets/style/style.css';
+import Navbar from "./components/Navbar";
+import bg from "./assets/img/bg.png"
+import {HeaderBg, HeaderContainer} from "./assets/style/styled";
 
 const App = () => {
+
+    const navLinks = [
+        {
+            name: 'Home',
+            link: '/'
+        },
+        {
+            name: 'Movies',
+            link: '/movies'
+        },
+        {
+            name: 'Series',
+            link: '/series'
+        },
+        {
+            name: "Last Added",
+            link: '/last-added'
+        },
+        {
+            name: "Most Popular",
+            link: '/most-popular'
+        }
+    ]
+
   return (
-    <div>
-      Hello world.
-    </div>
+    <>
+        <Navbar navLinks={navLinks}/>
+        <HeaderContainer>
+            <HeaderBg src={bg} alt={"Background"}/>
+        </HeaderContainer>
+    </>
   );
 }
 
