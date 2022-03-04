cb106d7a1843141d01f777de7c8b7b394e3eb93e
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-04 12:04:27 +0200

    Header oluşturuldu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
-import {HeaderBg, HeaderContainer} from "./style";
-import bg from "../../assets/img/bg.png";
+import {
+    HeaderBtn, HeaderBtnIcon,
+    HeaderButtonContainer,
+    HeaderContainer,
+    HeaderContent,
+    HeaderContentContainer,
+    HeaderContentImg, HeaderDesc
+} from "./style";
+import bg from "../../assets/img/bg2.jpg";
+import contentLogo from "../../assets/img/lcdpLogo.png"
+import sabaflixOriginal from "../../assets/img/original.svg"
+import sabaflixSeries from "../../assets/img/series.svg"
+import {SabaflixLogo} from "../../assets/style/styled";
+import shoppingBag from "../../assets/img/shoppingBag.svg";
+import infoIcon from "../../assets/img/info.svg"
 
 const Header = () => {
-    return(
-        <HeaderContainer>
-            <HeaderBg src={bg} alt={"Background"}/>
+    return (
+        <HeaderContainer bg={bg}>
+            <HeaderContentContainer>
+                <HeaderContent>
+                    <SabaflixLogo src={sabaflixSeries}/>
+                    <HeaderContentImg src={contentLogo} alt={"Logo"}/>
+                </HeaderContent>
+                <HeaderDesc>
+                    Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind
+                    manipulates the police to carry out his plan.
+                </HeaderDesc>
+
+                <HeaderButtonContainer>
+                    <HeaderBtn> <HeaderBtnIcon src={shoppingBag}/> Buy</HeaderBtn>
+                    <HeaderBtn opacity={true}><HeaderBtnIcon src={infoIcon}/>More Info</HeaderBtn>
+                </HeaderButtonContainer>
+            </HeaderContentContainer>
         </HeaderContainer>
     )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,13 +1,65 @@
 import styled from "styled-components";
+import {colors} from "../../assets/style/styled";
 
-export const HeaderContainer = styled.div`
-  width: 100%;
+export const HeaderContainer = styled.section`
   height: 100vh;
+  background: url(${props => props.bg}) no-repeat top center;
+  box-shadow: inset 670px -81px 134px rgba(0, 0, 0, 0.6);
+  background-size: cover;
+  position: relative;
 `
 
-export const HeaderBg = styled.img`
-height: 100%;
-  width: 100%;
-  object-fit: cover;
-  object-position: top;
+export const HeaderContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  position: absolute;
+  top: 30%;
+  left: 5%;
+`
+
+export const HeaderContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 1%;
+`
+
+export const HeaderContentImg = styled.img`
+  width: 40%;
+`
+
+export const HeaderDesc = styled.h1`
+  font-size: 22px;
+  color: ${colors.textPrimary};
+  margin-bottom: 1rem;
+  font-family: "Netflix Sans", sans-serif;
+  width: 50%;
+`
+
+export const HeaderButtonContainer = styled.div`
+  width: 50%;
+  display: flex;
+  margin-top: 10px;
+  gap: 40px;
+`
+
+export const HeaderBtn = styled.a`
+  background: ${ p => p.opacity ?  colors.whiteOpacity : colors.white };
+  color: ${p => p.opacity ? colors.white : colors.secondaryColor};
+  padding: 10px 20px;
+  border-radius: 4px;
+  font-size: 22px;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  font-family: "Netflix Sans Medium", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const HeaderBtnIcon = styled.img`
+  width: 30px;
+  margin-right: 10px;
 `
