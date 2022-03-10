95b7bb62d15a6b8f9fbece7bb3b0fb2171128dde
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-10 13:35:59 +0200

    ufak düzenlemeler

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -54,22 +54,22 @@ export const HeaderContent = styled.div`
 `
 
 export const HeaderContentImg = styled.img`
-  width: 40%;
+  max-width: 40%;
 
   @media (max-width: 768px) {
-    width: 40%;
+    max-width: 40%;
   }
 
   @media (min-width: 768px) {
-    width: 50%;
+    max-width: 50%;
   }
 
   @media (min-width: 992px) {
-    width: 70%;
+    max-width: 70%;
   }
   
   @media (min-width: 1200px) {
-    width: 40%;
+    max-width: 30%;
   }
 `
 
