3fdc3ce9067f552ecca2611837ae4468d00a1d9e
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-10 10:34:05 +0200

    popular bölümü eklendi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import LastAdded from "./components/LastAdded/LastAdded";
 import Footer from "./components/Footer/Footer";
+import Popular from "./components/Popular/Popular";
 
 const App = () => {
   return (
     <>
         <Navbar/>
         <Header/>
+        <Popular/>
         <LastAdded/>
         <Footer/>
     </>
