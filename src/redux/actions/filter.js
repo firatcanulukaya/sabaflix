5b673ce75c476a092b54615e68bc60447efdf540
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-25 11:44:02 +0200

    filtre ekleme

--- src/redux/actions/filter.js
+++ src/redux/actions/filter.js
@@ -1,5 +1,12 @@
-import {GET_CATEGORIES} from "../types";
+import {SET_FILTER, GET_CATEGORIES} from "../types";
 import axios from "axios";
+
+export const setFilter = (filter) => {
+    return {
+        type: SET_FILTER,
+        payload: filter
+    }
+};
 
 export const getCategories = () => dispatch =>{
     axios.get("http://10.80.0.168:8080/contentCategory")
