9005f7792a4fc2ae627461a9c1f7618bc00403b7
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-31 15:37:52 +0300

    arama kutusu linke yönlendiriliyor

--- src/components/Navbar/SearchBox.jsx
+++ src/components/Navbar/SearchBox.jsx
@@ -2,13 +2,16 @@ import {NavInput} from "./style";
 import {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
 import {getSearch} from "../../redux/actions/search";
+import {useNavigate} from "react-router-dom";
 
 const SearchBox = () => {
     const [searchValue, setSearchValue] = useState("");
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(getSearch(searchValue))
+        navigate(`/search?${searchValue}`)
     }, [dispatch, searchValue]);
 
     return (
