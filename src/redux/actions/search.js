72476f3bc357542f6f6dcfc04ec10b10a32c6c2a
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-03-31 14:12:26 +0300

    link düzeltildi

--- src/redux/actions/search.js
+++ src/redux/actions/search.js
@@ -3,7 +3,7 @@ import {GET_SEARCH} from "../types";
 
 export const getSearch = data => dispatch => {
     try {
-        axios.get(`http://10.80.0.168:8080/c?search=${data}`)
+        axios.get(`http://10.80.0.168:8080/content/title/c?search=${data}`)
             .then(res => {
                 if (res.status === 200) {
                     dispatch({
