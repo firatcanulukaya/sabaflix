198f8fb71d7a385ffbc7c56c8b163760069d11f8
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-04-07 14:40:10 +0300

    footer'a erol eklendi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 &copy; Sabaflix {new Date().getFullYear()}
             </FootterText>
             <FootterText>
-                Made with <span role="img" aria-label="heart">❤️ & ☕</span> by <FootterLink href="https://frtcn.net" target="_blank">Fıratcan Ulukaya</FootterLink>
+                Made with <span role="img" aria-label="heart">❤️ & ☕</span> by <FootterLink href="https://frtcn.net" target="_blank">Fıratcan Ulukaya</FootterLink> & <FootterLink href="https://grexlin85.com" target="_blank">Erol Umut Atalay</FootterLink>
             </FootterText>
         </Footter>
     )
